f7407e5d6f020950d5d30142f0882d0c387755ab
hs <63457118+kousakuin-A@users.noreply.github.com>
2021-02-28 22:50:38 +0900

    バグあり

--- yacht/cui.py
+++ yacht/cui.py
@@ -1,5 +1,7 @@
-from typing import Dict, Sequence
+from typing import Any, Dict, Sequence
 import random
+from porker_hand import Aces, Deuces, Threes, Fours, Fives, Sixes, \
+    Choice, FourOfAKind, FullHouse, ShortStraight, LongStraight, Yacht
 
 
 class ScoreBoard:
@@ -7,10 +9,10 @@ class ScoreBoard:
     '''
 
     def __init__(self, hands):
-        self.board: Dict[str, int] = {}
-        self.hands: Sequence[str] = hands
+        self.board: Dict[Any, int] = {}
+        self.hands: Sequence[Any] = hands
 
-    def set_score(self, hand: str, score: int):
+    def set_score(self, hand: Any, score: int):
         if hand not in self.hands:
             raise ValueError
         if hand in self.board:
@@ -24,7 +26,7 @@ class ScoreBoard:
                 score_to_show = self.board[hand]
             else:
                 score_to_show = 0
-            view_string += f"{hand:_<10}{score_to_show:_>4}\n"
+            view_string += f"{hand.name:_<10}{score_to_show:_>4}\n"
         total_score = sum(self.board.values())
         view_string += f"合計        {total_score:_>4}\n"
         return view_string
@@ -35,8 +37,9 @@ def roll_dices():
 
 
 if __name__ == '__main__':
-    HANDS = ['ヨット', 'ビッグストレート', 'スモールストレート', 'フォーナンバーズ', 'フルハウス',
-             'チョイス', 'シックス', 'ファイブ', 'フォー', 'スリー', 'デュース', 'エース']
+    HANDS = [Aces(), Deuces(), Threes(), Fours(), Fives(), Sixes(),
+             Choice(), FourOfAKind(), FullHouse(), ShortStraight(),
+             LongStraight(), Yacht()]
     board = ScoreBoard(HANDS)
     while True:
         print(board)
@@ -45,12 +48,15 @@ if __name__ == '__main__':
         dices = roll_dices()
         print(f'Dices:{dices}')
         print(f'Possible Hands:')
-        possible_hands = [(hand, random.randint(10, 100))
-                          for hand in ('スリー', 'デュース', 'エース')]
+        for hand in HANDS:
+            print(hand, end='')
+            print(hand.score(dices))
+        possible_hands = [(hand, hand.score(dices))
+                          for hand in HANDS if hand.score(dices) > 0]
         for i, hand in enumerate(possible_hands):
-            print(f'    [{i}]  {hand[0]:_<10}{hand[1]:_>4}')
+            print(f'    [{i}]  {hand[0].name:_<10}{hand[1]:_>4}')
         while True:
-            print(f'Choose one hand[input number]', end='>')
+            print('Choose one hand[input number]', end='>')
             try:
                 idx = int(input())
                 hand_to_set = possible_hands[idx]
